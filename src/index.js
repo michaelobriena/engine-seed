7a7e8b5f60ff0f54f8e0605ba1219885a2b6bd7e
Mike O'Brien <michael.obrien.a@gmail.com>
2015-06-09 15:20:44 -0700

    chore: more work

--- src/index.js
+++ src/index.js
@@ -3,36 +3,68 @@
 // Famous dependencies
 var DOMElement = require('famous/dom-renderables/DOMElement');
 var FamousEngine = require('famous/core/FamousEngine');
+var Grid = require('famous/layouts/Grid');
+var HeaderFooter = require('famous/layouts/HeaderFooter');
 
 // Boilerplate code to make your life easier
 FamousEngine.init();
 
-// Initialize with a scene; then, add a 'node' to the scene root
-var logo = FamousEngine.createScene().addChild();
+// // // // GRID // // // 
+// var grid = new Grid({
+//     dimensions: [2, 2],
+//     verticalSpacing: 100,
+//     horizontalSpacing: 100
+// });
 
-// Create an [image] DOM element providing the logo 'node' with the 'src' path
-new DOMElement(logo, { tagName: 'img' })
-    .setAttribute('src', './images/famous_logo.png');
+// FamousEngine.createScene().addChild(grid);
 
-// Chainable API
-logo
-    // Set size mode to 'absolute' to use absolute pixel values: (width 250px, height 250px)
-    .setSizeMode('absolute', 'absolute', 'absolute')
-    .setAbsoluteSize(250, 250)
-    // Center the 'node' to the parent (the screen, in this instance)
-    .setAlign(0.5, 0.5)
-    // Set the translational origin to the center of the 'node'
-    .setMountPoint(0.5, 0.5)
-    // Set the rotational origin to the center of the 'node'
-    .setOrigin(0.5, 0.5);
+// var children = grid.getChildren();
+// console.log(children)
+// for (var i = 0; i < children.length; i++) {
+//     new DOMElement(children[i], {
+//         content: i,
+//         properties: {
+//             backgroundColor: get_random_color()
+//         }
+//     });
+// }
 
-// Add a spinner component to the logo 'node' that is called, every frame
-var spinner = logo.addComponent({
-    onUpdate: function(time) {
-        logo.setRotation(0, time / 1000, 0);
-        logo.requestUpdateOnNextTick(spinner);
+// // // // HF // // // 
+var hf = new HeaderFooter({
+    headerSize: 50,
+    footerSize: 200
+});
+
+FamousEngine.createScene().addChild(hf);
+new DOMElement(hf.getHeader(), {
+    content: 'HEADER',
+    properties: {
+        backgroundColor: 'yellow'
     }
 });
 
-// Let the magic begin...
-logo.requestUpdate(spinner);
+new DOMElement(hf.getBody(), {
+    content: 'body',
+    properties: {
+        backgroundColor: 'blue'
+    }
+});
+
+new DOMElement(hf.getFooter(), {
+    content: 'FOOTER',
+    properties: {
+        backgroundColor: 'red'
+    }
+});
+
+
+function rand(min, max) {
+    return min + Math.random() * (max - min);
+}
+
+function get_random_color() {
+    var h = rand(1, 360);
+    var s = rand(0, 100);
+    var l = rand(0, 100);
+    return 'hsl(' + h + ',' + s + '%,' + l + '%)';
+}
